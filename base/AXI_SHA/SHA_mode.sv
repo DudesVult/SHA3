b6a65c597ee9a8d8d4784dad73e3b6675aee0193
DudesVult <DudesVult@yandex.ru>
2024-05-18 00:28:52 +0300

    Ничего не помогает

--- base/AXI_SHA/SHA_mode.sv
+++ base/AXI_SHA/SHA_mode.sv
@@ -18,6 +18,7 @@ module SHA_mode #(
    logic [7:0] cnt1;
    logic [7:0] cnt2;
    logic [1599:0] Dreg;
+   logic [(1600/DATA_WIDTH)-1:0][DATA_WIDTH-1:0] Dreg2;
    logic [5:0] lite_lim;
    logic [63:0] rev1; 
    logic [63:0] rev2; 
@@ -54,6 +55,7 @@ always_ff @(posedge ACLK) begin
         VALID <= 1'b0;
     end
     else if(cnt1 > 3) begin 
+        VALID <= 1'b1;
         if (Mode == 1'b1)
             if (cnt2 == lite_lim-1) 
                 Last <= 1'b1;
@@ -68,19 +70,47 @@ always_ff @(posedge ACLK) begin
     end
 end
 
-always_ff @(posedge ACLK) begin
-    if (cnt1 == 4)
-        VALID <= 1'b1;
-end
-
 generate
     for(genvar i = 0; i<(1600/DATA_WIDTH); i++) begin
-        always @(posedge ACLK) begin
-            if (cnt2 == i)                                                         // Bounded
-                Dout <= (Ready == 1) ? Dreg [DATA_WIDTH*(i+1)-1:DATA_WIDTH*i] : 0;
+        always @(posedge ACLK) begin                                                      // Bounded
+            if (VALID) begin
+                Dout <= Dreg2[0];
+                Dreg2 <= Dreg2 << 1;
+            end
         end
     end
 endgenerate
+
+generate
+    for(genvar j = 0; j<25; j++) begin
+        always @(posedge ACLK) begin
+        if (DATA_WIDTH == 8) begin
+            Dreg2[8*j]   <= rev[7:0];
+            Dreg2[8*j+1] <= rev[15:8];
+            Dreg2[8*j+2] <= rev[23:16];
+            Dreg2[8*j+3] <= rev[31:24];
+            Dreg2[8*j+4] <= rev[39:32];
+            Dreg2[8*j+5] <= rev[47:40];
+            Dreg2[8*j+6] <= rev[55:48];
+            Dreg2[8*j+7] <= rev[53:56];
+        end
+        if (DATA_WIDTH == 16) begin
+            Dreg2[4*j]   <= Din [15:0];
+            Dreg2[4*j+1] <= Din [31:16];
+            Dreg2[4*j+2] <= Din [47:32];
+            Dreg2[4*j+3] <= Din [63:48];
+        end
+        if (DATA_WIDTH == 32) begin
+            Dreg2[2*j]   <= rev[31:0];
+            Dreg2[2*j+1] <= rev[63:32];
+        end
+        if (DATA_WIDTH == 64) begin
+            Dreg2[j]   <= rev;
+        end
+    end
+    end
+endgenerate
+
 
 // byte_reversal in HW
 
@@ -98,11 +128,11 @@ generate
     for(genvar j = 1; j<26; j++) begin
         always @(posedge ACLK) begin
             if (cnt1 == j-1)
-            rev1 <= (Ready == 0) ? 64'b0 : Din [(25-j)%5] [(25-j)/5];
+            rev1 <= Din [(25-j)%5] [(25-j)/5];
             rev2 <= ((rev1<<8)   & 64'hFF00FF00FF00FF00)|((rev1>>8)  & 64'h00FF00FF00FF00FF);
             rev3 <= ((rev2<<16)   & 64'hFFFF0000FFFF0000)|((rev2>>16)  & 64'h0000FFFF0000FFFF);
             rev4 <= ((rev3<<32)   & 64'hFFFFFFFF00000000)|((rev3>>32)  & 64'h00000000FFFFFFFF);
-            Dreg [64*j-1:64*(j-1)] <= (cnt1 == j+2) ? rev : Dreg [64*j-1:64*(j-1)];
+//            Dreg [64*j-1:64*(j-1)] <= (cnt1 == j+2) ? rev : Dreg [64*j-1:64*(j-1)];
         end
     end
 endgenerate
